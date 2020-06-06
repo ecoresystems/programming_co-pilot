9dc7f52c5be01e4db7a9c703dcece407920e072b
umi <det_conan@live.cn>
2020-06-07 01:42:29 +0900

    Try to make sql work

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-from flask import Flask, request,jsonify
+from flask import Flask, request, jsonify
+from utilities import db_util
+import json
 
 app = Flask(__name__)
 
 
 @app.route('/api/get_recommendation', methods=['GET'])
 def hello_world():
-    compiler_output = request.args.get('comp_output')
-    content = request.args.get('content')
-    language = request.args.get('language')
-    return jsonify(compiler_output=compiler_output,content= content, language=language)
+    # compiler_output = request.args.get('comp_output')
+    # content = request.args.get('content')
+    # language = request.args.get('language')
+    json_file = open("sample_data.json",'r')
+    for row in json.load(json_file):
+        compiler_output = row['comp_output']
+        data1, data2, data3, data4 = db_util.err_msg_match(compiler_output)
+    return jsonify(content=data2, d4=data4)
     # return "hello world"
 
 
---
+++ utilities/db_util.py
@@ -0,0 +1,21 @@
+import mysql.connector
+import pandas as pd
+import time
+import sqlalchemy
+
+
+def err_msg_match(err_msg: str):
+    cnx = mysql.connector.connect(user='sotorrent', password='so-admin',
+                                  host='192.168.17.253',
+                                  database='sotorrent20_03')
+    err_type = err_msg.split('\n')[-1]
+    sql = sqlalchemy.text("SELECT AcceptedAnswerId,Title,Body FROM PythonPosts WHERE Body LIKE '%%s%' AND AnswerCount>3 AND AcceptedAnswerId IS NOT NULL"%err_type)
+    print(sql)
+    start_time = time.time()
+    question_df = pd.read_sql(sql, cnx,params={"err_type":err_type})
+    question_query_end_time = time.time()
+    answer_sql = "SELECT * FROM Posts WHERE Id IN %s" % str(tuple(list(question_df['AcceptedAnswerId'])))
+    answer_df = pd.read_sql(answer_sql, cnx)
+    answer_query_end_time = time.time()
+    cnx.close()
+    return question_df, question_query_end_time - start_time, answer_df, answer_query_end_time - question_query_end_time
