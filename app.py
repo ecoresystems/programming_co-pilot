c216917fa2b840523237203d41cf5be9345045c3
umi <det_conan@live.cn>
2020-06-07 00:36:51 +0900

    Add some basic routing

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, request,jsonify
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/api/get_recommendation', methods=['GET'])
 def hello_world():
-    return 'Hello World!'
+    compiler_output = request.args.get('comp_output')
+    content = request.args.get('content')
+    language = request.args.get('language')
+    return jsonify(compiler_output=compiler_output,content= content, language=language)
+    # return "hello world"
 
 
 if __name__ == '__main__':
